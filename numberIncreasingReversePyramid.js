978ee9f7c482d3c273ae6dc80ebce58b381fe7e0
CodesOfSachin <codes.of.sachin@gmail.com>
2025-09-29 01:55:42 +0530

    reverse number increasing pyramid implementation

---
+++ numberIncreasingReversePyramid.js
@@ -0,0 +1,12 @@
+let noOfRow = 4;
+
+function reverseNumberPyramid(noOfRow) {
+    for(let i = noOfRow; i > 0; i--) {
+    let str = ""
+    for(let j = 1; j <= i; j++) {
+        str += j
+    }
+    console.log(str)
+}}
+
+reverseNumberPyramid(noOfRow)
