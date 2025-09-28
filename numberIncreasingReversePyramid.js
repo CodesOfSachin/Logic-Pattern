840993d44d0505d83ff787ee78797318db08f941
CodesOfSachin <codes.of.sachin@gmail.com>
2025-09-29 02:03:03 +0530

    Fix spacing in reverse number pyramid output

--- numberIncreasingReversePyramid.js
+++ numberIncreasingReversePyramid.js
@@ -4,7 +4,7 @@ function reverseNumberPyramid(noOfRow) {
     for(let i = noOfRow; i > 0; i--) {
     let str = ""
     for(let j = 1; j <= i; j++) {
-        str += j
+        str += j + " "
     }
     console.log(str)
 }}
