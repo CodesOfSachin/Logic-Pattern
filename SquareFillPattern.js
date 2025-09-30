218199d327397b1edcc8c391e83e61b5e538fd32
CodesOfSachin <codes.of.sachin@gmail.com>
2025-10-01 03:02:42 +0530

    Square Fill star pattern minor mistake fix

--- SquareFillPattern.js
+++ SquareFillPattern.js
@@ -4,7 +4,7 @@ function squareFillPattern(n) {
     for(let i = 1; i <= n; i++) {
         let str = ""
         for(let j = n; j >= 1; j--) {
-            str += " *"
+            str += "* "
         }
         console.log(str)
     }
